b5b5b9847bfb061688c1916ba06ce125b0d4e7d5
attyhor <attyhor@gmail.com>
2021-04-02 10:55:40 +0200

    tabella manager flask + python 3.8

--- website/models.py
+++ website/models.py
@@ -1,11 +1,12 @@
 from flask_login import UserMixin
-from sqlalchemy.sql import func
+# from sqlalchemy.sql import func
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, ForeignKey, create_engine, or_
+from sqlalchemy import Column, Integer, String, ForeignKey, create_engine
 from sqlalchemy.orm import sessionmaker, relationship
 import datetime
 
 Base = declarative_base()
+
 
 class User(Base, UserMixin):
     __tablename__ = "users"
@@ -14,6 +15,8 @@ class User(Base, UserMixin):
     password = Column(String(150))
     first_name = Column(String(150))
     last_name = Column(String(150))
+    ACTIVE = Column(Integer, nullable=False, default=1)
+
 
 class Team(Base):
     __tablename__ = "teams"
@@ -23,11 +26,37 @@ class Team(Base):
     ACTIVE = Column(Integer, nullable=False, default=1)
     CREATEDON = Column(String, nullable=False, default=datetime.datetime.now)
     UPDATEON = Column(String, onupdate=datetime.datetime.now)
+    ACTIVE = Column(Integer, nullable=False, default=1)
+
+
+class Season(Base):
+    __tablename__ = "seasons"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255))
+
+
+class League(Base):
+    __tablename__ = "league"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(255))
+    # competition_id = Column(ForeignKey("competition.id"), nullable=False)
+    rounds = Column(Integer, nullable=False)
+    win_points = Column(Integer, nullable=False)
+    draw_points = Column(Integer, nullable=False)
+    lost_points = Column(Integer, nullable=False)
+    teams_number = Column(Integer, nullable=False)
+    seasons_id = Column(ForeignKey("seasons.id"), nullable=False)
+    ACTIVE = Column(Integer, nullable=False, default=1)
+
+    # competition_name = relationship(Competition, foreign_keys=[competition_id])
+    seasons_name = relationship(Season, foreign_keys=[seasons_id])
+
 
 class Match(Base):
     __tablename__ = "matches"
     id = Column(Integer, primary_key=True)
     round = Column(Integer)
+    league_id = Column(ForeignKey("league.id"), nullable=False)
     home_id = Column(ForeignKey("teams.id"), nullable=False)
     guest_id = Column(ForeignKey("teams.id"), nullable=False)
     home_score = Column(Integer, nullable=False)
@@ -40,28 +69,7 @@ class Match(Base):
 
     home_team = relationship(Team, foreign_keys=[home_id])
     guest_team = relationship(Team, foreign_keys=[guest_id])
-
-class Season(Base):
-    __tablename__ = "seasons"
-    id = Column(Integer, primary_key=True)
-    name = Column(String(255))
-
-class League(Base):
-    __tablename__ = "leagues"
-    id = Column(Integer, primary_key=True)
-    name = Column(String(255))
-    competition_id = Column(ForeignKey("competition.id"), nullable=False)
-    rounds = Column(Integer, nullable=False)
-    win_points = Column(Integer, nullable=False)
-    draw_points = Column(Integer, nullable=False)
-    lost_points = Column(Integer, nullable=False)
-    teams_number = Column(Integer, nullable=False)
-    seasons_id = Column(ForeignKey("seasons.id"), nullable=False)
-
-    competition_name = relationship(Competition, foreign_keys=[competition_id])
-    seasons_name = relationship(Season, foreign_keys=[seasons_id])
-
-
+    league_name = relationship(League, foreign_keys=[league_id])
 
 
 engine = create_engine('sqlite:///database.db', connect_args={'check_same_thread': False})
@@ -71,13 +79,4 @@ Session = sessionmaker(bind=engine)
 
 db = Session()
 
-
-"""
-    cursor.execute("CREATE TABLE IF NOT EXISTS team (id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY, league_id INT(11) NOT NULL,
-    CONSTRAINT FOREIGN KEY (league_id) REFERENCES league (id) ON DELETE CASCADE ON UPDATE RESTRICT, team_name VARCHAR(255)) ENGINE=InnoDB")
-    cursor.execute("CREATE TABLE IF NOT EXISTS `game` (`id` INT(11) NOT NULL AUTO_INCREMENT, `league_id` INT(11) NOT NULL,
-    `game_type_id` INT(11) NOT NULL, `home_team_id` INT(11) NOT NULL, `visiting_team_id` INT(11) NOT NULL, `round_of_game` INT(11) NOT NULL,
-    `date_of_game` DATE, `home_team_score` INT(11) NOT NULL, `visitor_team_score` INT(11) NOT NULL, `result_of_game` VARCHAR(1) NOT NULL,
-    PRIMARY KEY (`id`) USING BTREE, INDEX `league_id` (`league_id`) USING BTREE, INDEX `game_type_id_ibfk_1` (`game_type_id`) USING BTREE,
-    CONSTRAINT `game_type_id_ibfk_1` FOREIGN KEY (`game_type_id`) REFERENCES `league_manager`.`game_type` (`id`) ON UPDATE RESTRICT ON DELETE CASCADE, CONSTRAINT `league_id_ibfk_1` FOREIGN KEY (`league_id`) REFERENCES `league_manager`.`league` (`id`) ON UPDATE RESTRICT ON DELETE CASCADE, CONSTRAINT `home_team_id_ibfk_1` FOREIGN KEY (`home_team_id`) REFERENCES `league_manager`.`team` (`id`) ON UPDATE RESTRICT ON DELETE CASCADE, CONSTRAINT `visiting_team_id_ibfk_1` FOREIGN KEY (`visiting_team_id`) REFERENCES `league_manager`.`team` (`id`) ON UPDATE RESTRICT ON DELETE CASCADE  )  COLLATE='utf8_general_ci'  ENGINE=InnoDB  ;")
-"""
+# datetime.datetime.strptime("21/12/2008", "%d/%m/%Y").strftime("%Y-%m-%d")
